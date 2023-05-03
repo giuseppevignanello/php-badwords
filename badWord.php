63fe279bef9dfd060685a2ac8b0823944cf7f578
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-03 16:42:02 +0200

    add style with bootstrap

--- badWord.php
+++ badWord.php
@@ -13,34 +13,43 @@ $paragraphLen = strlen($paragraph);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
     <title>Document</title>
 </head>
 
 <body>
 
-    <h3>Paragraph</h3>
-    <p>
+    <div class="container">
+        <header class="text-center text-white bg-danger py-2">
+            <h1>The censor</h1>
+        </header>
+        <h3 class="text-center"> Paragraph</h3>
+        <p>
+            <?php
+            echo ($paragraph);
+            echo ($paragraphLen);
+            ?>
+
+        </p>
+
+        <p>
+        <h3 class="text-center"> Paragraph lenght</h3>
         <?php
-        echo ($paragraph);
         echo ($paragraphLen);
         ?>
+        </p>
 
-    </p>
+        <p>
+        <h3 class="text-center"> Censored paragraph</h3>
+        <?php
+        $badWord = $_GET["badWord"];
+        echo str_replace($badWord, "***", $paragraph)
+            ?>
+        </p>
+    </div>
 
-    <p>
-    <h3> Paragraph lenght</h3>
-    <?php
-    echo ($paragraphLen);
-    ?>
-    </p>
 
-    <p>
-    <h3> Censored paragraph</h3>
-    <?php
-    $badWord = $_GET["badWord"];
-    echo str_replace($badWord, "***", $paragraph)
-        ?>
-    </p>
 
 </body>
 
--- index.php
+++ index.php
@@ -5,27 +5,36 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet'
+        integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
     <title>Document</title>
 </head>
 
 <body>
-    <form action="badWord.php" method="get">
-        <div>
-            <label for="paragraph">Insert a paragraph</label>
-            <input type="text" name="paragraph" id=paragraph>
-        </div>
-        <div>
-            <label for="badWord">Select a Bad Word</label>
-            <input type="text" name="badWord" id=badWord>
-        </div>
-        <div>
-            <button type="submit">
-                Submit
-            </button>
-        </div>
+    <header class="text-center text-white py-2 bg-danger">
+        <h1>The Censor</h1>
+    </header>
+
+    <div class="container">
+        <form action="badWord.php" method="get">
+            <div class="my-2">
+                <label for="paragraph">Insert a paragraph</label>
+                <input type="text" name="paragraph" id=paragraph>
+            </div>
+            <div class="my-2">
+                <label for="badWord">Select a Bad Word</label>
+                <input type="text" name="badWord" id=badWord>
+            </div>
+            <div class="my-2">
+                <button class="btn-danger btn" type="submit">
+                    Submit
+                </button>
+            </div>
 
 
-    </form>
+        </form>
+    </div>
+
 
 </body>
 
